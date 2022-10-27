b12a4b554ec69e0afe5902b61360ce46e512313e
Ali Jalili Parsa <88662860+myselfajp@users.noreply.github.com>
2022-10-27 18:19:58 +0300

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class type(models.Model):
+    name=models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class post(models.Model):
+    type = models.ManyToManyField(type)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='Blog/',default='Blog/default.jpg')
+    discription = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
