93279b1e74729d30d57fb78bc66b0bf402fe145b
Ali Jalili Parsa <88662860+myselfajp@users.noreply.github.com>
2022-10-28 18:12:04 +0300

    about_us page finished

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import http_index
+from .views import http_index,http_aboup_us
 
 app_name="mysite"
 
 urlpatterns = [
     path("",http_index,name='index'),
+    path("about_us",http_aboup_us,name='about_us'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from posts.models import Team,Services,Projects,References
 
 def http_index(request):
-    team = Team.objects.all()
+    team = Team.objects.all()[0:4]
     projects = Projects.objects.all()
     services = Services.objects.all()
     references = References.objects.all()
@@ -14,3 +14,11 @@ def http_index(request):
         "references" : references,
     }
     return render(request,"index.html",context=context)
+
+
+def http_aboup_us(request):
+    team = Team.objects.all()
+    context={
+        "team":team,
+    }
+    return render(request,"about_us.html",context=context)
