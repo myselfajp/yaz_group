262f5a15bded42ca74d9fc46c717ce642098d59b
Ali Jalili Parsa <88662860+myselfajp@users.noreply.github.com>
2022-11-02 13:30:54 +0300

    Update settings.py
    change admin panel language and time zone to turkey

--- core/settings.py
+++ core/settings.py
@@ -95,9 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr-TR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
 
 USE_I18N = True
 
